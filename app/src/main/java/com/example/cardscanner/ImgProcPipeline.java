2100963d1cbc12caaddf4f4fb58af6e7fc785623
bothaynahazem <bothayna.hazem@gmail.com>
2019-05-02 23:09:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/cardscanner/ImgProcPipeline.java
@@ -0,0 +1,348 @@
+package com.example.cardscanner;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.support.v4.util.Pair;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.opencv.android.Utils;
+import org.opencv.core.*;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.objdetect.CascadeClassifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImgProcPipeline {
+
+    private static String TAG = ImgProcPipeline.class.getSimpleName();
+
+    public static Bitmap currentBitmap;
+    private static Bitmap croppedImgBitmap;
+    private static Mat scannedCardMat;
+
+    public static File loadClassifierData(Context context) {
+
+        File faceClassifierFile = null;
+        InputStream is;
+        FileOutputStream os;
+
+
+        try {
+            //loading the XML file of the classifier (the trained data)
+            is = context.getResources().openRawResource(R.raw.haarcascade_frontalface_alt);
+            File cascadeDir = context.getDir("cascade", context.MODE_PRIVATE);
+            faceClassifierFile = new File(cascadeDir, "haarcascade_frontalface_alt.xml");
+
+            os = new FileOutputStream(faceClassifierFile);
+
+            byte[] buffer = new byte[4096];
+            int bytesRead;
+            while ((bytesRead = is.read(buffer)) != -1) {
+                os.write(buffer, 0, bytesRead);
+            }
+
+            is.close();
+            os.close();
+
+        } catch (IOException e) {
+            Log.i(TAG, "Face cascade not found");
+        }
+
+        return faceClassifierFile;
+    }
+
+
+    public static void detectFaceInCard(Mat src, Mat dst, Context currentContext) {
+
+        //creates the face classifier xml file to be used by the face detection method
+        File faceClassifierFile = loadClassifierData(currentContext);
+
+        CascadeClassifier faceDetector = new CascadeClassifier(faceClassifierFile.getAbsolutePath());
+
+        if (faceDetector.empty()) {
+
+            Log.e(TAG, "****** Cascade Classifier Wasn't loaded ******");
+        } else {
+            Log.i(TAG, "Loaded cascade classifier from "
+                    + faceClassifierFile.getAbsolutePath());
+        }
+
+        //gray img --> equalize histogram --> input to the classifier
+        Mat grayFrame = new Mat();
+        Imgproc.cvtColor(src, grayFrame, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.equalizeHist(grayFrame, grayFrame);
+
+        MatOfRect facesDetected = new MatOfRect();
+        faceDetector.detectMultiScale(grayFrame, facesDetected); //the detected faces are returned in facesDetected
+
+        if (facesDetected.empty()) {
+            Toast.makeText(currentContext, "No faces detected in this card", Toast.LENGTH_SHORT);
+            Log.e(TAG, "Successfully scanned but no faces detected!");
+            return;
+        }
+
+
+        Rect[] faces = facesDetected.toArray();
+        Rect rect = faces[0].clone();
+
+        // This loop is for when there're faces detected from the patterns in the card
+        // same as when one sees a face in the clouds
+        if (faces.length > 1) {
+            for (Rect face : faces) {
+                int fw = face.width;
+                int sw = src.width();
+                if (face.width >= (src.width() * 0.1))
+                    rect = face.clone();
+            }
+        }
+
+        int newX = rect.x - (int) (rect.width * 0.4);
+        int newY = rect.y - (int) (rect.height * 0.4);
+        int newWidth = (int) (rect.width * 1.8);
+        int newHeight = (int) (rect.height * 1.8);
+
+        if (newX + newWidth > src.width()) //the cropped part will surpass the edges of the card
+        {
+            newWidth = src.width() - newX;
+        }
+
+        if (newY + newHeight > src.height()) //not necessary for the faculty's cards but could be for other types of cards
+        {
+            newHeight = src.height() - newY;
+        }
+
+
+        Rect theCroppedFace = new Rect(newX, newY, newWidth, newHeight);
+
+        Mat faceImgOnly = new Mat(src, theCroppedFace);
+
+        faceImgOnly.copyTo(dst);
+
+    }
+
+
+    private static void resizeHeight(Mat src, Mat dst, double height) {
+        int w = src.width();
+        int h = src.height();
+        double r = height / (float) (h);
+        Size dim = new Size((int) (w * r), height);
+        Imgproc.resize(src, dst, dim, Imgproc.INTER_AREA);
+    }
+
+
+    private static Pair<Point, Integer> getMinX(ArrayList<Point> Points) {
+        Point tempX = Points.get(0).clone();
+        int index = 0;
+        for (int i = 0; i < Points.toArray().length; i++) {
+            if (Points.get(i).x < tempX.x) {
+                tempX.x = Points.get(i).x;
+                tempX.y = Points.get(i).y;
+                index = i;
+            }
+        }
+        return new Pair<>(tempX, index);
+    }
+
+    private static MatOfPoint2f getFourBoundaryPts(Mat input_img, double NEW_HEIGHT) {
+
+        //Resize the img to be able to find the contours
+        double ratio = input_img.height() / NEW_HEIGHT;
+        resizeHeight(input_img, input_img, NEW_HEIGHT);
+
+        Mat gray_img = new Mat();
+        Mat blurred_img = new Mat();
+        Mat edges_img = new Mat();
+
+        /*  EDGES IMAGE (White pixels --> card edges, Black pixels --> Otherwise) */
+        Imgproc.cvtColor(input_img, gray_img, Imgproc.COLOR_BGR2GRAY);
+        Imgproc.GaussianBlur(gray_img, blurred_img, new Size(5, 5), 0);
+        Imgproc.Canny(blurred_img, edges_img, 70, 200);
+
+
+        /*  DEBUGGING to view the edges img bitmap  */
+        Bitmap edges = null;
+        edges = Bitmap.createBitmap(edges_img.width(), edges_img.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(edges_img, edges);
+        /*  DEBUGGING  */
+
+
+        /*   FINDING CONTOURS OF CARD   */
+        List<MatOfPoint> contours = new ArrayList<>();
+        Mat hierarchy = new Mat();
+        Imgproc.findContours(edges_img, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_SIMPLE);
+
+
+
+        MatOfPoint2f cardContour = new MatOfPoint2f();
+        for (MatOfPoint c : contours) {
+            double perimeter = Imgproc.arcLength(new MatOfPoint2f(c.toArray()), true);
+            MatOfPoint2f approximatePolygon = new MatOfPoint2f();
+            Imgproc.approxPolyDP(new MatOfPoint2f(c.toArray()), approximatePolygon, 0.02 * perimeter, true);
+
+            if (approximatePolygon.toArray().length == 4) {
+                cardContour = approximatePolygon;
+                break;
+            }
+        }
+
+
+        ArrayList<Point> resizedPoints = new ArrayList<>();
+        for (Point P : cardContour.toArray()) {
+            Point temp = new Point();
+            temp.x = P.x * ratio;
+            temp.y = P.y * ratio;
+
+            resizedPoints.add(temp);
+        }
+
+        /*  REARRANGING THE CORNER POINTS FOUND --> to be suitable for the next method  */
+        Point tempMinX1 = new Point();
+        int idx1 = 0;
+        Pair<Point, Integer> minX1;
+        minX1 = getMinX(resizedPoints);
+
+        tempMinX1 = minX1.first.clone();
+        idx1 = minX1.second;
+        resizedPoints.remove(idx1);
+
+
+        Point tempMinX2 = new Point();
+        int idx2 = 0;
+        Pair<Point, Integer> minX2;
+        minX2 = getMinX(resizedPoints);
+
+        tempMinX2 = minX2.first.clone();
+        idx2 = minX2.second;
+        resizedPoints.remove(idx2);
+
+
+        Point tl, bl, tr, br;
+        if (tempMinX1.y > tempMinX2.y) {
+            bl = tempMinX1;
+            tl = tempMinX2;
+        } else {
+            tl = tempMinX1;
+            bl = tempMinX2;
+        }
+
+        if (resizedPoints.get(0).y > resizedPoints.get(1).y) {
+            br = resizedPoints.get(0);
+            tr = resizedPoints.get(1);
+        } else {
+            tr = resizedPoints.get(0);
+            br = resizedPoints.get(1);
+        }
+
+        /*  The order accepted by the transformation fn is as follows: { tl, bl, br, tr }  */
+        ArrayList<Point> resizedPointsNew = new ArrayList<>();
+        resizedPointsNew.add(tl);
+        resizedPointsNew.add(bl);
+        resizedPointsNew.add(br);
+        resizedPointsNew.add(tr);
+
+
+        MatOfPoint2f result = new MatOfPoint2f();
+        result.fromList(resizedPointsNew);
+
+        if (result.size() == new Size(0, 0)) {
+            Log.e(TAG, "The MatOfPoint2f doesn't contain the pts.");
+        }
+
+        return result;
+    }
+
+    private static void transformCardToRect(Mat input_img, Mat output_img, MatOfPoint2f src, int FINAL_WIDTH, int FINAL_HEIGHT) {
+
+        // src(cardFourPtsContour is a matrix containing the corner points of the image to transform
+        //dst is a matrix containing the points that the corners in src are going to be mapped into
+
+        if (src.width() == 0 && src.height() == 0) //no pts were retrieved
+        {
+            Log.e(TAG, "No points were received from the \"getFourBoundaryPts\" method!");
+        }
+
+        int w = input_img.width();
+        int h = input_img.height();
+
+        MatOfPoint2f dst = new MatOfPoint2f(
+                new Point(0, 0), // tl
+                new Point(0, FINAL_HEIGHT), // bl
+                new Point(FINAL_WIDTH, FINAL_HEIGHT), // br
+                new Point(FINAL_WIDTH, 0) // tr
+
+        );
+
+        Mat P = new Mat();  //P is a temp variable
+        P = Imgproc.getPerspectiveTransform(src, dst);
+
+        //src –img1- input image.
+        //dst –output- output image that has the size dsize and the same type as src .
+        //M –P- 3\times 3 transformation matrix.
+        //size – size of the output image -  can be greater than the output image and corners around image will be black.
+        Imgproc.warpPerspective(input_img, output_img, P, new Size(FINAL_WIDTH, FINAL_HEIGHT));
+    }
+
+
+    private static void rotateImg(Mat src, Mat dst) {
+
+        if (src.cols() <= src.rows()) //already rotated
+        {
+            src.copyTo(dst); //hence don't rotate it
+        }
+
+        int angleNeg90 = 270; //when the img is taken in a "portrait mode", it's rotated by 270
+        double scale = 1.0;
+
+        Point center = new Point(src.cols() / 2.0, src.rows() / 2.0); //centre to be rotated about
+
+        Mat M = Imgproc.getRotationMatrix2D(center, angleNeg90, scale);
+
+        Size newSize = new Size(src.cols(), src.rows());
+        Imgproc.warpAffine(src, dst, M, newSize);
+
+    }
+
+
+    /*  For the OCR function  */
+    public static Bitmap getCroppedImg() {
+        Rect rectCrop = new Rect(180, 250, 520, 300);
+        Mat croppedImage = new Mat(scannedCardMat, rectCrop);
+
+        croppedImgBitmap = null;
+        croppedImgBitmap = Bitmap.createBitmap(croppedImage.width(), croppedImage.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(croppedImage, croppedImgBitmap);
+
+        return croppedImgBitmap;
+    }
+
+
+    /*  Specifying the New height resizing level as well as the final width and height of the card to be scanned  */
+    public static void scanCard(Mat inputImg, Mat outputImg, double NEW_HEIGHT, int FINAL_WIDTH, int FINAL_HEIGHT) {
+        Mat origImg = new Mat();
+        inputImg.copyTo(origImg);
+
+        Mat rotatedInputImg = new Mat();
+        rotateImg(inputImg, rotatedInputImg);
+
+        Mat rotatedOrigImg = new Mat();
+        rotateImg(inputImg, rotatedOrigImg);
+
+        MatOfPoint2f cardFourPtsContour = getFourBoundaryPts(rotatedInputImg, NEW_HEIGHT);
+
+        transformCardToRect(rotatedOrigImg, outputImg, cardFourPtsContour, FINAL_WIDTH, FINAL_HEIGHT);
+
+        currentBitmap = Bitmap.createBitmap(outputImg.width(), outputImg.height(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(outputImg, currentBitmap);
+
+        scannedCardMat = new Mat();
+        Bitmap bm = currentBitmap.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bm, scannedCardMat); //for OCR
+    }
+
+}
---
+++ app/src/main/java/com/example/cardscanner/tessOCR.java
@@ -0,0 +1,105 @@
+package com.example.cardscanner;
+
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.util.Log;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class tessOCR {
+
+    public static final String lang = "eng";
+
+    private static final String TAG = "TESSERACT";
+    private AssetManager assetManager;
+
+    private TessBaseAPI mTess;
+
+    public tessOCR(AssetManager assetManager) {
+
+        Log.i(TAG, MainActivity.DATA_PATH);
+
+        this.assetManager = assetManager;
+
+        String[] paths = new String[]{MainActivity.DATA_PATH, MainActivity.DATA_PATH + "/tessdata/"};
+
+        for (String path : paths) {
+            File dir = new File(path);
+            if (!dir.exists()) {
+                if (!dir.mkdirs()) {
+                    Log.v(TAG, "ERROR: Creation of directory " + path + " on sdcard failed");
+                    return;
+                } else {
+                    Log.v(TAG, "Created directory " + path + " on sdcard");
+                }
+            }
+        }
+
+        if (!(new File(MainActivity.DATA_PATH + "/tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                OutputStream out = new FileOutputStream(new File(MainActivity.DATA_PATH + "/tessdata/", lang + ".traineddata"));
+
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) != -1) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+
+        mTess = new TessBaseAPI();
+        mTess.setDebug(true);
+        mTess.init(MainActivity.DATA_PATH, lang);
+
+    }
+
+
+    public String getResults(Bitmap croppedImgBitmap) {
+        mTess.setImage(croppedImgBitmap);
+        String result = "";
+        String fullText = mTess.getUTF8Text();
+
+
+        String[] lines = fullText.split("\\n");
+        String line2;
+        if (lines.length > 1)
+            line2 = lines[1];
+
+        else if (lines.length == 1)
+            line2 = lines[0];
+
+        else
+            return null;
+
+        line2 = line2.replaceAll("\\s", "");
+        Pattern pattern = Pattern.compile("(1[0-9]{6})");
+//        Pattern pattern = Pattern.compile("([0-9]+):");
+        Matcher matcher = pattern.matcher(line2);
+
+        if (matcher.find()) {
+            result = matcher.group(1);
+        }
+
+        return result;
+    }
+
+    public void onDestroy() {
+        if (mTess != null)
+            mTess.end();
+    }
+}
